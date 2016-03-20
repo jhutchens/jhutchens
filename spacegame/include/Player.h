4116e6c65d3cf4cbf8bd3d17d237199586ca5950
jhutchens <jonlegohut@gmail.com>
2016-03-20 18:42:10 -0400

    added get/set methods & fuel/energy tracking

--- spacegame/include/Player.h
+++ spacegame/include/Player.h
@@ -28,13 +28,33 @@ class Player : public Character
         /** Default destructor */
         virtual ~Player();
 
+        //overrides the Character process() to include additional processing
+        void process();
+        void increaseSpeed();//overrides to include fuel consumption
+        void shoot();//shoot a laser beam
+        int getFuel();//returns current fuel level
+        int getShield();//returns current shield level
+        int getEnergy();//returns current energy level
+
+
+
     protected:
         //the laser's ammo is based on energy levels; therefore it is not in the weapons[] array
 
         int shield,energy;//shield and energy levels
+        int laserEnergy;//energy consumed when shooting laser
         int weapons[NUM_WEAPONS];//number/ammo of each weapon (where each weapons[x] corresponds to a different weapon)
-        double fuel;//amount of fuel left
+
+        //amount of fuel left, steady loss of fuel from engine running, & fuel used each frame for motion
+        int fuel,fuelLoss,fuelConsumed;
+
         bool hasMap;//has the player acquired a map? (default is false)
+
+
+
+        void setFuelLoss(int newFuelLoss);
+        void setFuelConsumed(int newRate);
+        void updateStocks();
 
 
     private:
--- spacegame/src/Player.cpp
+++ spacegame/src/Player.cpp
@@ -7,13 +7,111 @@
 Player::Player(SDL_Renderer &renderer,SDL_Texture &texture,int x,int y):Character(renderer,texture,x,y)
 {
     //constructor
-    shield=0;
-    energy=100;
+    shield=0;//starting shield level
+    energy=100;//starting energy level
+    laserEnergy=2;//starting cost of shooting laser
     for(int i=0;i<sizeof(weapons);i++){
         weapons[i]=0;//initialize all special weapons to have zero ammo
     }
-    fuel=100;
-    hasMap=false;
+    fuel=10000;//starting amount of fuel
+    fuelLoss=1;//constant fuel loss from engine running
+    fuelConsumed=30;//only taken into account when accelerating
+
+    //turnFuel=3;//fuel consumed for steering
+    //^prolly not a good idea - so that players can still aim & shoot even if out of fuel
+
+    hasMap=false;//no map to start
+}
+
+void Player::increaseSpeed()
+{
+    //can only accelerate if player has fuel left
+    if(fuel<fuelConsumed)
+    {
+        fuel=0;
+    }else
+    {
+        fuel-=fuelConsumed;
+        Character::increaseSpeed();
+    }
+
+}
+
+void Player::process()
+{
+    //update fuel & energy levels
+    updateStocks();
+    //printf("Player::process() function operating...\n");
+    Character::process();//then run normal processing
+}
+
+void Player::updateStocks()
+{
+    if(fuel<fuelLoss)
+    {
+        fuel=0;
+    }else
+    {
+        fuel-=fuelLoss;
+    }
+    //@todo update energy levels
+
+
+
+}
+
+void Player::shoot()
+{
+    if(laserEnergy<=energy)
+    {
+        //@todo shoot laser
+        energy-=laserEnergy;
+    }else
+    {
+        printf("Error: not enough energy to shoot laser! %d, %d\n",laserEnergy,energy);
+    }
+
+
+}
+
+//sets the rate of steady fuel loss (should be very low - less than 3)
+void Player::setFuelLoss(int newFuelLoss)
+{
+    if(newFuelLoss<0)
+    {
+        printf("Error: fuelLoss cannot be less than zero. \n");
+    }else
+    {
+        fuelLoss=newFuelLoss;
+    }
+
+}
+
+//sets the additional rate of fuel loss when accelerating (moderate to high)
+void Player::setFuelConsumed(int newRate)
+{
+    if(newRate<0)
+    {
+        printf("Error: fuelConsumed cannot be less than zero. \n");
+    }else
+    {
+        fuelConsumed=newRate;
+    }
+}
+
+int Player::getFuel()
+{
+    return fuel;
+}
+
+int Player::getShield()
+{
+    return shield;
+}
+
+int Player::getEnergy()
+{
+    return energy;
 }
 
 Player::~Player()
